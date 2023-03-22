4e8abeddf2b0569e01f22eaef8793fc4c7c6119d
oussama <oussama.sefiani@um5r.ac.ma>
2023-03-21 19:39:27 -0700

    0x02-functions_nested_loo 2p

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char text[10] = "Holberton";
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
 	_putchar('\n');
+
 	return (0);
 
