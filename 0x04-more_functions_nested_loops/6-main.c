de669d6612d878f4fe18c353e9e3bfad201cdd0f
oussama <oussama.sefiani@um5r.ac.ma>
2023-03-30 03:58:07 +0000

    fixed the buug

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_line(0);
+	print_line(2);
+	print_line(56);
+	print_line(-67);
+	return (0);
+}
