5f0947821869cbbb2cf1575ca625a176142f34a5
oussama <oussama.sefiani@um5r.ac.ma>
2023-04-17 12:27:08 +0000

    0-Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Short description
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: Who owns the dog
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
