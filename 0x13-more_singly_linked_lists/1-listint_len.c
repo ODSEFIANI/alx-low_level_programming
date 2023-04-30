3fcfb03ebd9a5267a7e0b04fb1db6dd6eef83e8b
oussama sefiani <oussama.sefiani@um5r.ac.ma>
2023-04-30 16:34:38 +0000

    Write a function that returns the number of elements in a linked listint_t list.

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,20 +2,19 @@
 #include <stdio.h>
 
 /**
- * print_listint - prints the elemnets of linked list(node by node)
+ * print_listint_len - prints the elemnets of linked list(node by node)
  * @h:constant pointer to a structer node
  *
  * Return:number of elments in linked list
  */
 
 
-size_t print_listint(const listint_t *h)
+size_t print_listint_len(const listint_t *h)
 {
 	unsigned int count = 0;
 
 	while (h)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
