eb51f330830cf8db8eb067b26e8fdfe1e047cac8
OUSSAMA SEFIANI <oussama.sefiani@um5r.ac.ma>
2023-08-24 01:07:57 +0100

    de

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+
+    Vcounter = 0
+    hcounter = 0
+
+    # Counting horizontally
+    for row in range(len(grid) -1):
+        for column in range(len(grid[row])-1):
+            if grid[row][column] == 1 and (grid[row][column +1] ==1 or grid[row ][column - 1] ==1):
+                hcounter = hcounter + 1
+
+    # Counting vertically
+    for row in range(len(grid ) -1):
+        for column in range(len(grid[row])-1):
+            if grid[row][column] == 1 and (grid[row +1][column] ==1 or grid[row -1][column] ==1):
+                Vcounter = Vcounter + 1
+           
+
+    return 2 *  (Vcounter + hcounter)
